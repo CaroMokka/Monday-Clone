cd423eb5a493a5c841584735f5775be1af1d3906
carolina <ci.arayagonzalez@gmail.com>
2022-08-09 19:26:53 -0400

    fixServer

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const url = 'https://54f61ce3-4d88-4d0b-bed8-af80d5f20689-europe-west1.apps.astra.datastax.com/api/rest/v2/namespaces/tickets/collections/tasks'
-const token = 'AstraCS:uwkjSbJPbSPUJfGLEAPPbbXT:a524aa5556410a8c7237196b1373dc29f7414b2ca5f4ae9b99c91faf67660aa9'
+const url = '...'
+const token = '...'
 
 app.get('/tickets', async (req, res) => {
     const options = {
